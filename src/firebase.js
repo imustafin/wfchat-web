d0e33056ed8a21c4e925f1d6f62481083c2bd0af
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-03-26 12:44:30 +0300

    Add firebase keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,10 @@
 import firebase from 'firebase';
-import firebaseKeys from '../firebaseKeys.json';
+
+const firebaseKeys = {
+  apiKey: "AIzaSyA0znKrxhs4kmljRdT2feQwwkmadfGq1O8",
+  authDomain: "wfchat-906e0.firebaseapp.com",
+  databaseURL: "https://wfchat-906e0.firebaseio.com"
+};
 
 firebase.initializeApp(firebaseKeys);
 
