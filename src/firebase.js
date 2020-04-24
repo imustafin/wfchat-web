04bab85ec0695e9f056ba9323b84a378de3f4dfd
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-04-24 20:51:49 +0300

    Add storage config

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,12 @@ import firebase from 'firebase';
 const firebaseKeys = {
   apiKey: "AIzaSyA0znKrxhs4kmljRdT2feQwwkmadfGq1O8",
   authDomain: "wfchat-906e0.firebaseapp.com",
-  databaseURL: "https://wfchat-906e0.firebaseio.com"
+  databaseURL: "https://wfchat-906e0.firebaseio.com",
+  storageBucket: 'wfchat-906e0.appspot.com'
 };
 
 firebase.initializeApp(firebaseKeys);
 
 export const auth = firebase.auth;
 export const db = firebase.database();
+export const storage = firebase.storage();
